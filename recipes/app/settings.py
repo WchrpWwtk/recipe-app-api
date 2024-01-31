181df4937702635e9aa457f229174128581ba6a1
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-01-31 14:53:17 +0700

    Configure URLs.

--- recipes/app/settings.py
+++ recipes/app/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3mzo(v++bfa2ir#j=&7lfh-likqf#+0o+ttu#3)63+m=(tu9*p"
+SECRET_KEY = (
+    "django-insecure-3mzo(v++bfa2ir#j=&7lfh-likqf#+0o+ttu#3)63+m=(tu9*p"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +132,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
