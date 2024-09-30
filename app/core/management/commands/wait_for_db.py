bc4c1c8b287d1b31699578db3b3bc57bff375746
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-09-30 10:42:31 +0700

    feat: Add custom command to wait for the database
    Implemented CoreConfig for the core app in Django. Added custom management command 'wait_for_db' to handle database availability checks with corresponding tests.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,34 @@
+"""
+Django command to wait for the database to be available.
+"""
+import time
+
+from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2OpError
+
+
+class Command(BaseCommand):
+    """
+    Django command to wait for a database.
+    """
+
+    def handle(self, *args, **options):
+        """
+        Entrypoint for command.
+        :param args:
+        :param options:
+        :return:
+        """
+        self.stdout.write("Waiting for the database...")
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
