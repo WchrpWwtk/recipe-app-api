b85784b1febd67a522868ae1d21022e1d261f079
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-09-30 14:16:40 +0700

    refactor: Replace flake8 with ruff for linting
    Removed flake8 configurations and dependencies, substituting them with ruff. Updated pre-commit hooks and CI workflows to integrate ruff for linting and formatting. Adjusted code style to reflect updated tooling preferences.

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j3d!k8)yy_e)k%)f8o64ifdv&11si3v$kdewp)g%o49m53)jeu"
+SECRET_KEY = (
+    "django-insecure-j3d!k8)yy_e)k%)f8o64ifdv&11si3v$kdewp)g%o49m53)jeu"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
