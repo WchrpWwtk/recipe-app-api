10565f0d38e3a61716abda4bbb77d353dc05f2a6
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-10-28 15:25:21 +0700

    feat: Add Tag model and integrate with Recipe
    Implemented a new Tag model for filtering recipes. Integrated tags into the Recipe model with many-to-many relationships and updated Django admin and settings to support this functionality. Added corresponding migration files and tests for the new Tag model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,17 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from recipe.models import Recipe
+from tag.models import Tag
+
+
+def create_user(email="user@example.com", password="testpass123"):
+    """
+    Create and return a new user.
+    :param email:
+    :param password:
+    :return:
+    """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -78,3 +89,9 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+        tag = Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
--- app/recipe/models.py
+++ app/recipe/models.py
@@ -20,6 +20,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField("tag.Tag")
 
     def __str__(self):
         return self.title
