6b9126bdaf22a667957813be3db49eda99c76f0b
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-10-28 13:13:08 +0700

    feat: Add detailed view for recipe API
    Refactored viewset to use different serializers for list and detail actions. Added RecipeDetailSerializer to serializers and tests for retrieving recipe details.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -3,6 +3,7 @@ Tests for recipe APIs.
 """
 
 from decimal import Decimal
+from typing import LiteralString
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -13,86 +14,107 @@ from rest_framework.test import APIClient
 
 from ..models import Recipe
 
-from ..serializers import RecipeSerializer
+from ..serializers import (
+	RecipeSerializer,
+	RecipeDetailSerializer,
+)
 
 RECIPE_URL = reverse("recipe:recipe-list")
 
 
+def detail_url(recipe_id):
+	"""
+	Create and return a recipe detail URL.
+	"""
+	return reverse("recipe:recipe-detail", args=[recipe_id])
+
+
 def create_recipe(user, **params):
-    """
-    Create and return a sample recipe.
-    :param user:
-    :param params:
-    :return:
-    """
-    defaults = {
-        "title": "Sample recipe title",
-        "time_minutes": 22,
-        "price": Decimal("5.25"),
-        "description": "Sample description",
-        "link": "http://example.com/recipe.pdf",
-    }
-    defaults.update(params)
+	"""
+	Create and return a sample recipe.
+	:param user:
+	:param params:
+	:return:
+	"""
+	defaults = {
+		"title": "Sample recipe title",
+		"time_minutes": 22,
+		"price": Decimal("5.25"),
+		"description": "Sample description",
+		"link": "http://example.com/recipe.pdf",
+	}
+	defaults.update(params)
 
-    recipe = Recipe.objects.create(user=user, **defaults)
+	recipe = Recipe.objects.create(user=user, **defaults)
 
-    return recipe
+	return recipe
 
 
 class PublicRecipeAPITests(TestCase):
-    """
-    Test unauthenticated recipe API requests.
-    """
+	"""
+	Test unauthenticated recipe API requests.
+	"""
 
-    def setUp(self):
-        self.client = APIClient()
+	def setUp(self):
+		self.client = APIClient()
 
-    def test_auth_required(self):
-        res = self.client.get(RECIPE_URL)
+	def test_auth_required(self):
+		res = self.client.get(RECIPE_URL)
 
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+		self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateRecipeApiTests(TestCase):
-    """
-    Test authenticated API requests.
-    """
+	"""
+	Test authenticated API requests.
+	"""
 
-    def setUp(self):
-        self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
-            "user@example.com",
-            "test_pass123",
-        )
-        self.client.force_authenticate(self.user)
+	def setUp(self):
+		self.client = APIClient()
+		self.user = get_user_model().objects.create_user(
+			"user@example.com",
+			"test_pass123",
+		)
+		self.client.force_authenticate(self.user)
 
-    def test_retrieve_recipes(self):
-        """
-        Test retrieving a list of recipes.
-        :return:
-        """
-        create_recipe(user=self.user)
-        create_recipe(user=self.user)
+	def test_retrieve_recipes(self):
+		"""
+		Test retrieving a list of recipes.
+		:return:
+		"""
+		create_recipe(user=self.user)
+		create_recipe(user=self.user)
 
-        res = self.client.get(RECIPE_URL)
+		res = self.client.get(RECIPE_URL)
 
-        recipes = Recipe.objects.all().order_by("-id")
-        serializer = RecipeSerializer(recipes, many=True)
+		recipes = Recipe.objects.all().order_by("-id")
+		serializer = RecipeSerializer(recipes, many=True)
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializer.data)
+		self.assertEqual(res.status_code, status.HTTP_200_OK)
+		self.assertEqual(res.data, serializer.data)
 
-    def test_recipe_list_limited_to_user(self):
-        other_user = get_user_model().objects.create_user(
-            "other@example.com", "password123"
-        )
-        create_recipe(user=other_user)
-        create_recipe(user=self.user)
+	def test_recipe_list_limited_to_user(self):
+		other_user = get_user_model().objects.create_user(
+			"other@example.com", "password123"
+		)
+		create_recipe(user=other_user)
+		create_recipe(user=self.user)
 
-        res = self.client.get(RECIPE_URL)
+		res = self.client.get(RECIPE_URL)
 
-        recipes = Recipe.objects.filter(user=self.user)
-        serializer = RecipeSerializer(recipes, many=True)
+		recipes = Recipe.objects.filter(user=self.user)
+		serializer = RecipeSerializer(recipes, many=True)
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializer.data)
+		self.assertEqual(res.status_code, status.HTTP_200_OK)
+		self.assertEqual(res.data, serializer.data)
+
+	def test_get_recipe_detail(self):
+		"""
+		Test get recipe detail.
+		"""
+		recipe = create_recipe(user=self.user)
+
+		url = detail_url(recipe.id)
+		res = self.client.get(url)
+		serializer = RecipeDetailSerializer(recipe)
+		self.assertEqual(res.data, serializer.data)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,18 +11,28 @@ from . import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    """
-    View for manage recipe APIs.
-    """
+	"""
+	View for manage recipe APIs.
+	"""
 
-    serializer_class = serializers.RecipeSerializer
-    queryset = Recipe.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+	serializer_class = serializers.RecipeDetailSerializer
+	queryset = Recipe.objects.all()
+	authentication_classes = [TokenAuthentication]
+	permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        """
-        Retrieve recipes for authenticated user.
-        :return:
-        """
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+	def get_queryset(self):
+		"""
+		Retrieve recipes for authenticated user.
+		:return:
+		"""
+		return self.queryset.filter(user=self.request.user).order_by("-id")
+
+	def get_serializer_class(self):
+		"""
+		Return the serializer class for the request.
+		"""
+
+		if self.action == "list":
+			return serializers.RecipeSerializer
+
+		return self.serializer_class
