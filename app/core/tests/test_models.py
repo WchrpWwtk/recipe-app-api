6ea6a6d793f412457738c76f25283895f635368e
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-10-29 16:53:49 +0700

    feat: Add image upload functionality to recipes
    Added an endpoint to upload images to recipes, with corresponding serializer and image handling. Included tests for image upload and error handling to ensure robustness.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,109 +16,109 @@ from unittest.mock import patch
 
 
 def create_user(email="user@example.com", password="testpass123"):
-	"""
-	Create and return a new user.
-	:param email:
-	:param password:
-	:return:
-	"""
-	return get_user_model().objects.create_user(email, password)
+    """
+    Create and return a new user.
+    :param email:
+    :param password:
+    :return:
+    """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
-	"""
-	Test models.
-	"""
+    """
+    Test models.
+    """
 
-	def test_create_user_with_email_successful(self):
-		"""
-		Test creating a user with an email is successful.
-		:return:
-		"""
-		email = "test@example.com"
-		password = "test_pass123"
-		user = get_user_model().objects.create_user(
-			email=email, password=password
-		)
+    def test_create_user_with_email_successful(self):
+        """
+        Test creating a user with an email is successful.
+        :return:
+        """
+        email = "test@example.com"
+        password = "test_pass123"
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
 
-		self.assertEqual(user.email, email)
-		self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
 
-	def test_new_user_email_normalized(self):
-		"""
-		Test email is normalized for new users.
-		:return:
-		"""
-		sample_emails = [
-			["test1@EXAMPLE.com", "test1@example.com"],
-			["Test2@Example.com", "Test2@example.com"],
-			["TEST3@EXAMPLE.COM", "TEST3@example.com"],
-			["test4@example.COM", "test4@example.com"],
-		]
+    def test_new_user_email_normalized(self):
+        """
+        Test email is normalized for new users.
+        :return:
+        """
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
 
-		for email, expected in sample_emails:
-			user = get_user_model().objects.create_user(email, "sample123")
-			self.assertEqual(user.email, expected)
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
 
-	def test_new_user_without_email_raises_error(self):
-		with self.assertRaises(ValueError):
-			get_user_model().objects.create_user("", "test123")
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
 
-	def test_create_superuser(self):
-		"""
-		Test creating a superuser.
-		:return:
-		"""
-		user = get_user_model().objects.create_superuser(
-			"test@example.com", "test123"
-		)
+    def test_create_superuser(self):
+        """
+        Test creating a superuser.
+        :return:
+        """
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "test123"
+        )
 
-		self.assertTrue(user.is_superuser)
-		self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
 
-	def test_create_recipe(self):
-		"""
-		Test creating a recipe is successful.
-		:return:
-		"""
-		user = get_user_model().objects.create_user(
-			"test@example.com", "test_pass123"
-		)
-		recipe = Recipe.objects.create(
-			user=user,
-			title="Sample recipe name",
-			time_minutes=5,
-			price=Decimal("5.50"),
-			description="Sample recipe description.",
-		)
+    def test_create_recipe(self):
+        """
+        Test creating a recipe is successful.
+        :return:
+        """
+        user = get_user_model().objects.create_user(
+            "test@example.com", "test_pass123"
+        )
+        recipe = Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description.",
+        )
 
-		self.assertEqual(str(recipe), recipe.title)
+        self.assertEqual(str(recipe), recipe.title)
 
-	def test_create_tag(self):
-		user = create_user()
-		tag = Tag.objects.create(user=user, name="Tag1")
+    def test_create_tag(self):
+        user = create_user()
+        tag = Tag.objects.create(user=user, name="Tag1")
 
-		self.assertEqual(str(tag), tag.name)
+        self.assertEqual(str(tag), tag.name)
 
-	def test_create_ingredient(self):
-		"""
-		Test creating an ingredient is successful.
-		:return:
-		"""
-		user = create_user()
-		ingredient = Ingredient.objects.create(user=user, name="Ingredient 1")
+    def test_create_ingredient(self):
+        """
+        Test creating an ingredient is successful.
+        :return:
+        """
+        user = create_user()
+        ingredient = Ingredient.objects.create(user=user, name="Ingredient 1")
 
-		self.assertEqual(str(ingredient), ingredient.name)
+        self.assertEqual(str(ingredient), ingredient.name)
 
-	@patch("core.models.uuid.uuid4")
-	def test_recipe_file_name_uuid(self, mock_uuid):
-		"""
-		Test generating an image path.
-		:param mock_uuid:
-		:return:
-		"""
-		uuid = "test-uuid"
-		mock_uuid.return_value = uuid
-		file_path = recipe_image_file_path(None, "example.jpg")
+    @patch("core.models.uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """
+        Test generating an image path.
+        :param mock_uuid:
+        :return:
+        """
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = recipe_image_file_path(None, "example.jpg")
 
-		self.assertEqual(file_path, f"uploads/recipe/{uuid}.jpg")
+        self.assertEqual(file_path, f"uploads/recipe/{uuid}.jpg")
