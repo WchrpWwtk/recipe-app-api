fe94d0024e5bab34aa6f07e278e8c2e78414af21
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-10-29 11:41:38 +0700

    feat: Add tag handling on recipe update
    Implemented functionality to handle tag creation and assignment when updating a recipe. Added tests to verify creating tags, assigning existing tags, and clearing tags on recipe updates.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -31,21 +31,50 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
 
-    def create(self, validated_data):
+    def _get_or_create_tags(self, tags, recipe):
         """
-        Create a recipe.
-        :param validated_data:
+        Handle getting or creating tags as needed.
+        :param tags:
+        :param recipe:
         :return:
         """
-        tags = validated_data.pop("tags", [])
-        recipe = Recipe.objects.create(**validated_data)
         auth_user = self.context["request"].user
 
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """
+        Create a recipe.
+        :param validated_data:
+        :return Recipe:
+        """
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """
+        Update recipe.
+        :param instance:
+        :param validated_data:
+        :return:
+        """
+        tags = validated_data.pop("tags", None)
+
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
