d86f1d061b41290acfd9b8fac58e854a88a54b4b
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-10-29 16:33:13 +0700

    feat: Add image field to Recipe model
    Added image field to the Recipe model allowing image uploads. Modified models, migrations, and tests to support the new image field functionality.

--- app/core/models.py
+++ app/core/models.py
@@ -3,60 +3,76 @@ Database models.
 """
 
 from django.contrib.auth.models import (
-    PermissionsMixin,
-    BaseUserManager,
-    AbstractBaseUser,
+	PermissionsMixin,
+	BaseUserManager,
+	AbstractBaseUser,
 )
 from django.db import models
 
+import os
+import uuid
+
+
+def recipe_image_file_path(instance, filename):
+	"""
+	Generate a file path for new recipe image.
+	:param instance:
+	:param filename:
+	:return:
+	"""
+	ext = os.path.splitext(filename)[1]
+	filename = f"{uuid.uuid4()}{ext}"
+
+	return os.path.join("uploads", "recipe", filename)
+
 
 class UserManager(BaseUserManager):
-    """
-    Manager for users.
-    """
+	"""
+	Manager for users.
+	"""
 
-    def create_user(self, email: str, password: str = None, **extra_fields):
-        """
-        Create, save and return a new user.
-        :param email:
-        :param password:
-        :param extra_fields:
-        :return:
-        """
-        if not email:
-            raise ValueError("User must have an email address.")
+	def create_user(self, email: str, password: str = None, **extra_fields):
+		"""
+		Create, save and return a new user.
+		:param email:
+		:param password:
+		:param extra_fields:
+		:return:
+		"""
+		if not email:
+			raise ValueError("User must have an email address.")
 
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
+		user = self.model(email=self.normalize_email(email), **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
 
-        return user
+		return user
 
-    def create_superuser(self, email: str, password: str):
-        """
-        Create and return a new superuser.
-        :param email:
-        :param password:
-        :return:
-        """
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
+	def create_superuser(self, email: str, password: str):
+		"""
+		Create and return a new superuser.
+		:param email:
+		:param password:
+		:return:
+		"""
+		user = self.create_user(email, password)
+		user.is_staff = True
+		user.is_superuser = True
+		user.save(using=self._db)
 
-        return user
+		return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """
-    User in the system.
-    """
+	"""
+	User in the system.
+	"""
 
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+	email = models.EmailField(max_length=255, unique=True)
+	name = models.CharField(max_length=255)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
 
-    objects = UserManager()
+	objects = UserManager()
 
-    USERNAME_FIELD = "email"
+	USERNAME_FIELD = "email"
--- app/recipe/models.py
+++ app/recipe/models.py
@@ -5,6 +5,8 @@ Recipe models.
 from django.conf import settings
 from django.db import models
 
+from core.models import recipe_image_file_path
+
 
 class Recipe(models.Model):
     """
@@ -22,6 +24,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("tag.Tag")
     ingredients = models.ManyToManyField("ingredient.Ingredient")
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
