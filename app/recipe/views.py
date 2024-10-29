d34c80a3d12515e521a2162cc8356eca7236fe32
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-10-29 14:58:22 +0700

    refactor: Create BaseRecipeAttrViewSet for recipe attributes
    Consolidate common functionality of TagViewSet and IngredientViewSet into a new BaseRecipeAttrViewSet. This reduces redundancy and simplifies the code by reusing authentication and permission logic.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -49,47 +49,40 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(
+class BaseRecipeAttrViewSet(
     mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
+    """
+    Base view set for recipe attributes.
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Filter queryset to authenticate user.
+        :return:
+        """
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
     """
     Manage tags in the database.
     """
 
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """
-        Filter queryset to authenticate user.
-        :return:
-        """
-        return self.queryset.filter(user=self.request.user).order_by("-name")
 
 
-class IngredientViewSet(
-    mixins.DestroyModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """
     Manage ingredients in the database.
     """
 
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """
-        Filter queryset to authenticate user.
-        :return:
-        """
-        return self.queryset.filter(user=self.request.user).order_by("-name")
